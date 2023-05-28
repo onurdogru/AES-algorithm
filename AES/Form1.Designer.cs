6502565a594b4a12e3f5a9674aaf902dc9891ba3
Onur <onurdogruiletisim@gmail.com>
2023-05-28 23:56:10 +0300

    Add project files.

---
+++ AES/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace AES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(176, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(611, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Advanced Encrption Standard (AES)";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(489, 363);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(137, 28);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Hidden Info";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Violet;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(699, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Purple;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.OldLace;
+            this.button2.Location = new System.Drawing.Point(699, 522);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 39);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Key Open";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightCoral;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(699, 474);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 42);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Key";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkRed;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.Gold;
+            this.button4.Location = new System.Drawing.Point(699, 567);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 42);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Data Read";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Gold;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(43, 79);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(424, 251);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.SeaGreen;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox2.Location = new System.Drawing.Point(489, 79);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(424, 251);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox3.ForeColor = System.Drawing.Color.White;
+            this.richTextBox3.Location = new System.Drawing.Point(43, 347);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(424, 251);
+            this.richTextBox3.TabIndex = 11;
+            this.richTextBox3.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(489, 429);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(951, 644);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "AES - ONUR DOĞRU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AES/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        aesSifrecoz AES_Ogrendik = new aesSifrecoz(); //BU CLASS'IN tüm özelliklerini buraya taşımış oluruz.
+
+        public class INIKaydet
+        {
+
+            [DllImport("kernel32")]
+            private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+            [DllImport("kernel32")]
+            private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+            public INIKaydet(string dosyaYolu)
+            {
+                DOSYAYOLU = dosyaYolu;
+            }
+            private string DOSYAYOLU = String.Empty;
+            public string Varsayilan { get; set; }
+            public string Oku(string bolum, string ayaradi)
+            {
+                Varsayilan = Varsayilan ?? string.Empty;
+                StringBuilder StrBuild = new StringBuilder(256);
+                GetPrivateProfileString(bolum, ayaradi, Varsayilan, StrBuild, 255, DOSYAYOLU);
+                return StrBuild.ToString();
+            }
+            public long Yaz(string bolum, string ayaradi, string deger)
+            {
+                return WritePrivateProfileString(bolum, ayaradi, deger, DOSYAYOLU);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            INIKaydet ini = new INIKaydet(Application.StartupPath + @"\Ayarlar.ini");
+            ini.Yaz("Gizli Bilgi", "Gizli Metin Kutusu", richTextBox3.Text);
+            ini.Yaz("Gizli CheckBox", "Gizli Bilgi", checkBox1.Checked.ToString());
+            MessageBox.Show("Ayarlar kayıt altına alındı");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(Application.StartupPath + @"\Ayarlar.ini"))
+                {
+                    INIKaydet ini = new INIKaydet(Application.StartupPath + @"\Ayarlar.ini");
+                    richTextBox3.Text = ini.Oku("Gizli Bilgi", "Gizli Metin Kutusu"); // [Gizli Bilgi] Gizli Metin Kutus = "bıla bıla"
+                    checkBox1.Checked = Convert.ToBoolean(ini.Oku("Gizli CheckBox", "Gizli Bilgi"));
+                }
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("ini dosyası hasarlı" + hata.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(Application.StartupPath + @"\Ayarlar.ini"))
+                {
+                    INIKaydet ini = new INIKaydet(Application.StartupPath + @"\Ayarlar.ini");
+                    richTextBox3.Text = ini.Oku("Gizli Bilgi", "Gizli Metin Kutusu"); // [Gizli Bilgi] Gizli Metin Kutus = "bıla bıla"
+                    checkBox1.Checked = Convert.ToBoolean(ini.Oku("Gizli CheckBox", "Gizli Bilgi"));
+                }
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("ini dosyası hasarlı" + hata.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = AES_Ogrendik.AESsifrele(richTextBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(richTextBox2.Text == string.Empty)
+            {
+                MessageBox.Show("Çözülecek Şifre Bulunamadı");
+            }
+            else
+            {
+                richTextBox3.Text = AES_Ogrendik.AESsifre_Coz(richTextBox2.Text);
+            }
+            
+        }
+    }
+}
